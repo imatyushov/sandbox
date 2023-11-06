8bacd6ef5efa432978840e494296424f12e154b2
Ilya <80617082+imatyushov@users.noreply.github.com>
2023-11-06 21:25:44 +0300

    Promise.race()

--- promise-polyfill/my-app/src/App.tsx
+++ promise-polyfill/my-app/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './PromiseConstructor';
 import './PromiseTest';
 
 const App = () => {
+    const [seconds, setSeconds] = useState<number>(0);
+    const [isStarted, setIsStarted] = useState<boolean>(false);
+
+    useEffect(() => {
+        let intervalId: string | number | NodeJS.Timeout | undefined;
+        if (isStarted) {
+            intervalId = window.setInterval(() => {
+                setSeconds((prevState) => prevState + 1);
+            }, 1_000);
+        }
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, [isStarted]);
 
   return (
       <div>
-          РАБОТАЕТ
+          <p>{seconds}</p>
+          <button onClick={() => setIsStarted(true)}>Start</button>
+          <button onClick={() => setIsStarted(false)}>Stop</button>
       </div>
   );
 };
--- promise-polyfill/my-app/src/PromiseTest.tsx
+++ promise-polyfill/my-app/src/PromiseTest.tsx
@@ -1,72 +1,116 @@
 
-export const promise = new Promise<number>((resolve, reject) => {
-    let timeoutId = setTimeout(() => {
-        resolve(2);
-    }, 1_000);
-});
+// export const promise = new Promise<number>((resolve, reject) => {
+//     let timeoutId = setTimeout(() => {
+//         resolve(2);
+//     }, 1_000);
+// });
+//
+//
+// promise
+//     .then((resolve) => resolve * 2)
+//     .then((resolve) => resolve * 2)
+//     .catch((error) => {
+//         console.error(error);
+//         return 1_000;
+//     })
+//     .then(resolve => console.log(resolve))
+//
+//
 
 
-promise
-    .then((resolve) => resolve * 2)
-    .then((resolve) => resolve * 2)
-    .catch((error) => {
-        console.error(error);
-        return 1_000;
-    })
-    .then(resolve => console.log(resolve))
 
 
-const sleep = new Promise((resolve) =>
-    setTimeout(() => resolve, 2_000));
+// export const firstPromise = new Promise<number>((res, rej) => {
+//     setTimeout(() => res(10), 2_000 );
+// })
+// export const secondPromise = new Promise<string>((res, rej) => {
+//     setTimeout(() => {
+//
+//     }, )
+//     res('success');
+// })
+// export const thirdPromise = new Promise<string>((res, rej) => {
+//     res('good job');
+// })
+//
+// export function sleep(ms: number) {
+//     return new Promise((resolve) => setTimeout(() => resolve, ms))
+// }
 
+// Promise.all([firstPromise, secondPromise, thirdPromise])
+//     .then((resolve) => console.log(resolve))
+//     .catch((err) => console.error('Error:', err));
+//
+// Promise.allSettled([firstPromise, secondPromise, thirdPromise])
+//     .then((resolve) => console.log(
+//             resolve.map((value, index, array) => ({
+//                 value,
+//                 index,
+//                 array
+//             })))
+//     )
+//     .catch((err) => console.error('Error:', err));
+//
+// export function promiseAll<T>(promises: (Promise<number> | Promise<string>)[]) {
+//     const resultsArray: any[] = [];
+//     let count = 0;
+//     return new Promise((resolve, reject) => {
+//         for (let index = 0; index < promises.length; index++) {
+//             promises[index]
+//                 .then((res) => {
+//                     count++
+//                     resultsArray[index] = (res);
+//
+//                     if (promises.length === count) {
+//                         resolve(resultsArray);
+//                     }
+//             })
+//                 .catch((error) => {
+//                 reject(error);
+//             });
+//         }
+//     });
+// }
+//
+// const customPromiseAll = promiseAll([firstPromise, secondPromise, thirdPromise]);
+//
+//
+// console.log(customPromiseAll);
+
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 export const firstPromise = new Promise<number>((res, rej) => {
-    // setTimeout(() => res(10), 5_000 );
-    res(1);
+    setTimeout(() => res(10), 3000 );
 })
 export const secondPromise = new Promise<string>((res, rej) => {
-    res('success');
+    setTimeout(() => {
+        res('success');
+        }, 2000)
 })
 export const thirdPromise = new Promise<string>((res, rej) => {
-    res('good job');
+    setTimeout(() => {
+        res('good job');
+    }, 1000)
 })
 
-Promise.all([firstPromise, secondPromise, thirdPromise])
-    .then((resolve) => console.log(resolve))
-    .catch((err) => console.error('Error:', err));
+// Promise.race([firstPromise, secondPromise, thirdPromise])
+//     .then((resolve) => {
+//         console.log(resolve);
+// })
 
-Promise.allSettled([firstPromise, secondPromise, thirdPromise])
-    .then((resolve) => console.log(
-            resolve.map((value, index, array) => ({
-                value,
-                index,
-                array
-            })))
-    )
-    .catch((err) => console.error('Error:', err));
 
-export function promiseAll<T>(promises: (Promise<number> | Promise<string>)[]) {
-    const resultsArray: any[] = [];
-    let count = 0;
+function promiseRace<T>(promises: (Promise<number> | Promise<string>)[]) {
     return new Promise((resolve, reject) => {
-        for (let index = 0; index < promises.length; index++) {
-            promises[index]
-                .then((res) => {
-                    count++
-                    resultsArray[index] = (res);
-
-                    if (promises.length === count) {
-                        resolve(resultsArray);
-                    }
-            })
-                .catch((error) => {
-                reject(error);
-            });
-        }
-    });
+        promises.forEach((promise) => {
+            Promise.resolve(promise)
+                .then(resolve, reject)
+                .catch(reject)
+        })
+    })
 }
 
-const customPromiseAll = promiseAll([firstPromise, secondPromise, thirdPromise]);
 
 
-console.log(customPromiseAll);
+promiseRace([firstPromise, secondPromise, thirdPromise]).then((res) => console.log(res));
+
