9e2f43faa8cc8a8d96c0c180e4ae14ceb53b44c9
Ilya <80617082+imatyushov@users.noreply.github.com>
2023-11-11 23:08:14 +0300

    Undo/Redo

--- sync-async/my-app/src/App.tsx
+++ sync-async/my-app/src/App.tsx
@@ -1,5 +1,73 @@
+import { useState, useReducer } from 'react';
 
 export function App() {
-  console.log('✅')
-  return <div>Work!</div>
+    const [events, setEvents] = useState([]);
+
+    const [stack, dispatch] = useReducer((acc, val) => {
+        switch(val.action) {
+            case 'push':
+                return [...acc, val];
+            case 'pop':
+                return acc.slice(0, acc.length - 1);
+            default:
+                return [...acc];
+        }
+    }, []);
+
+    const [border, setBorder] = useState(undefined);
+
+    function makeEvent() {
+        setEvents([...events, `Event ${events.length + 1}`]);
+        setBorder(stack[stack.length - 1]);
+    }
+
+    function undo() {
+        const eventsFiltered = events.filter((e) => !stack.some((s) => s === e));
+        dispatch({
+            action: 'push',
+            payload: eventsFiltered[eventsFiltered.length - 1]
+        })
+    }
+
+    function redo() {
+        if(stack[stack.length - 1] === border) {
+            return;
+        }
+        dispatch({
+            action: 'pop'
+        });
+    }
+
+    const resultStack = events.filter((event) => !stack.some((s) => s === event));
+
+    return (
+        <>
+        <div className={'App'}>
+            <div style={{display: 'flex', gap: 10}}>
+                <div>Border: {border}</div>
+            </div>
+            <h3>Events</h3>
+            {events.map((event) => {
+                return <div>{event}</div>
+            })}
+            <div>
+                <h3>Undo Stack</h3>
+                {stack.map((event) => {
+                    return <div>{event}</div>
+                })}
+            </div>
+            <div>
+                <h3>Result Stack</h3>
+                {resultStack.map((event) => {
+                    return <div>{event}</div>
+                })}
+            </div>
+        </div>
+            <div>
+                <button onClick={makeEvent}>Make event</button>
+                <button onClick={undo}>undo</button>
+                <button onClick={redo}>redo</button>
+            </div>
+        </>
+    )
 }
